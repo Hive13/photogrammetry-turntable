17426dc17a27a4b34cea5830a4e7f1d897324811
Dave Schwinn <braveness23@gmail.com>
2021-06-19 11:45:03 -0400

    initial commit of example esp32-framework sketch

---
+++ main.ino
@@ -0,0 +1,15 @@
+/* stack initialize the services in setup and
+ * should serve in loop continueously
+ */
+
+#include <EwingsEspStack.h>
+
+void setup()
+{
+    EwStack.initialize();
+}
+
+void loop()
+{
+    EwStack.serve();
+}
